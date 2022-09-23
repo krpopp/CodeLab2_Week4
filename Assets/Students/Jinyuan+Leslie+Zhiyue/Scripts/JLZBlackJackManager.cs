d6c8d0827795db5ea4a44266727c903a6eee0d19
lesliesun016 <leslieyiyuanzeng@gmail.com>
2022-09-23 16:36:46 -0400

    add the 4 files

---
+++ Assets/Students/Jinyuan+Leslie+Zhiyue/Scripts/JLZBlackJackManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JLZBlackJackManager : BlackJackManager
+{
+
+	public Text statusText;
+	public GameObject tryAgain;
+	public string loadScene;
+
+
+	// notify that the player busts
+	public void PlayerBusted(){
+		HidePlayerButtons();
+		GameOverText("YOU BUST", Color.red);
+	}
+
+	// notify the dealer busts
+	public void DealerBusted(){
+		GameOverText("DEALER BUSTS!", Color.green);
+	}
+		
+	// notify the player wins
+	public void PlayerWin(){
+		GameOverText("YOU WIN!", Color.green);
+	}
+		
+	// notify the player loses
+	public void PlayerLose(){
+		GameOverText("YOU LOSE.", Color.red);
+	}
+
+	// notify "black jack" and hide the player button 
+	public void BlackJack(){
+		GameOverText("Black Jack!", Color.green);
+		HidePlayerButtons();
+	}
+
+	// set the game over text active
+	public void GameOverText(string str, Color color){
+		statusText.text = str;
+		statusText.color = color;
+
+		tryAgain.SetActive(true);
+	}
+
+	// disable the hit button and stay button 
+	public void HidePlayerButtons(){
+		GameObject.Find("HitButton").SetActive(false);
+		GameObject.Find("StayButton").SetActive(false);
+	}
+
+	// reset the game
+	public void TryAgain(){
+		SceneManager.LoadScene(loadScene);
+	}
+
+	// get the sum of card value
+	public virtual int GetHandValue(List<DeckOfCards.Card> hand){
+		int handValue = 0;
+
+		foreach(DeckOfCards.Card handCard in hand){
+			handValue += handCard.GetCardHighValue();
+		}
+		return handValue;
+	}
+}
---
+++ Assets/Students/Jinyuan+Leslie+Zhiyue/Scripts/JLZDealerHand.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class JLZDealerHand : DealerHand{
+
+	public Sprite cardBack;
+
+	bool reveal;
+
+	// hide the image and text of the card
+	protected override void SetupHand(){
+		base.SetupHand();
+
+		GameObject cardOne = transform.GetChild(0).gameObject;
+		cardOne.GetComponentInChildren<Text>().text = "";
+		cardOne.GetComponentsInChildren<Image>()[0].sprite = cardBack;
+		cardOne.GetComponentsInChildren<Image>()[1].enabled = false;
+
+		reveal = false;
+	}
+		
+	protected override void ShowValue(){
+
+		// if the dealer has more than 1 card
+		if(hand.Count > 1){
+			// if not revealled, show one card info in the dealer's hand
+			if(!reveal){
+				handVals = hand[1].GetCardHighValue();
+
+				total.text = "Dealer: " + handVals + " + ???";
+			} else {
+				// if revealled, show all values in the dealer's hand
+				handVals = GetHandValue();
+
+				total.text = "Dealer: " + handVals;
+
+				BlackJackManager manager = GameObject.Find("Game Manager").GetComponent<BlackJackManager>();
+
+				// if the dealer's cards' value is more than 21, the dealer busts
+				if(handVals > 21){
+					manager.DealerBusted();
+				} else if(!DealStay(handVals)){ // if the value is less than 17, the dealer draws card
+					Invoke("HitMe", 1);
+				} else { // if the value is between 17 and 21
+					BlackJackHand playerHand = GameObject.Find("Player Hand Value").GetComponent<BlackJackHand>();
+					// if player card's value is more than the dealer's, plaer wins; otherwise, dealer wins
+					if(handVals < playerHand.handVals){
+						manager.PlayerWin();
+					} else {
+						manager.PlayerLose();
+					}
+				}
+			}
+		}
+	}
+
+	// check if the value is larger than 17
+	protected virtual bool DealStay(int handVal){
+		return handVal > 17;
+	}
+
+	// reveal the hidden card info of dealer
+	public void RevealCard(){
+		reveal = true;
+
+		GameObject cardOne = transform.GetChild(0).gameObject;
+
+		cardOne.GetComponentsInChildren<Image>()[0].sprite = null;
+		cardOne.GetComponentsInChildren<Image>()[1].enabled = true;
+
+		ShowCard(hand[0], cardOne, 0);
+
+		ShowValue();
+	}
+}
