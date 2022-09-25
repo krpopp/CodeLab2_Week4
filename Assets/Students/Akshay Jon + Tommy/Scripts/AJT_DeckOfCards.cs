92e5d2d0b3fe810fa028f15440e9f7065b047d10
Akshay <akshaybharadhwaj98@gmail.com>
2022-09-25 13:59:30 -0400

    Updated AJT_Deck of Cards

--- Assets/Students/Akshay Jon + Tommy/Scripts/AJT_DeckOfCards.cs
+++ Assets/Students/Akshay Jon + Tommy/Scripts/AJT_DeckOfCards.cs
@@ -4,39 +4,93 @@ using UnityEngine;
 
 public class AJT_DeckOfCards : DeckOfCards
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	 new public class Card{
+		
+		//enums to hold a suite
+		public enum Suit {
+			SPADES, 	//0
+			CLUBS,		//1
+			DIAMONDS,	//2
+			HEARTS	 	//3
+		};
+		
+		//enums to hold card face values
+		public enum Type {
+			TWO		= 2,
+			THREE	= 3,
+			FOUR	= 4,
+			FIVE	= 5,
+			SIX		= 6,
+			SEVEN	= 7,
+			EIGHT	= 8,
+			NINE	= 9,
+			TEN		= 10,
+			J		= 11,
+			Q		= 12,
+			K		= 13,
+			A		= 14
+		};
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		public Type cardNum;
 
-	public int GetCardLowValue()
-	{                   //Checks the face value on the card and in case of A,J,K or Q it assigns it a value
-		int val;                                    //in case of it being a number it defaults to the number on it
+		public Suit suit;			//variable of type Suit
 
-		switch (cardNum)
-		{
+		public Card(Type cardNum, Suit suit){		//constructor to declare a variable of type Card
+			this.cardNum = cardNum;
+			this.suit = suit;
+		}
+
+		public override string ToString(){				//overrides the ToString function
+			return "The " + cardNum + " of " + suit;
+		}
+
+		//BUG; write GetCardLowValue
+		public int GetCardHighValue(){					//Checks the face value on the card and in case of A,J,K or Q it assigns it a value
+			int val;									//in case of it being a number it defaults to the number on it
+
+			switch(cardNum){
 			//BUG; ace should be 11 or 1
 			case Type.A:
-				val = 1;
+				val = 11;
 				break;
 			case Type.K:
 			case Type.Q:
 			case Type.J:
 				val = 10;
-				break;
+				break;	
 			default:
 				val = (int)cardNum;
 				break;
+			}
+
+			return val;
 		}
 
-		return val;
+		public int GetCardLowValue()
+		{   
+
+			GetCardHighValue();
+							//Checks the face value on the card and in case of A,J,K or Q it assigns it a value
+			int val;                                    //in case of it being a number it defaults to the number on it
+
+			switch (cardNum)
+			{
+				//BUG; ace should be 11 or 1
+				case Type.A:
+					val = 1;
+					break;
+				case Type.K:
+				case Type.Q:
+				case Type.J:
+					val = 10;
+					break;
+				default:
+					val = (int)cardNum;
+					break;
+			}
+
+			return val;
+		}
 	}
 
 }
