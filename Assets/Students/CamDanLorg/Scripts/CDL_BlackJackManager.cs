e335165a42c2e3d53b21f0cf087b2a8f137f3771
Lorg <hikariame100@gmail.com>
2022-09-26 18:01:55 -0400

    CDL; ace value fix

--- Assets/Students/CamDanLorg/Scripts/CDL_BlackJackManager.cs
+++ Assets/Students/CamDanLorg/Scripts/CDL_BlackJackManager.cs
@@ -12,4 +12,27 @@ public class CDL_BlackJackManager : BlackJackManager
         HidePlayerButtons();
         GameOverText("DEALER BUSTS!", Color.green); //displays game over tex.t
     }*/
+
+    //Bug 1: Ace only has one value. (should 1 or 11). - FIXED
+    public override int GetHandValue(List<DeckOfCards.Card> hand) //we basically add a check that in case the handvalue is larger than 21 
+    //we try to find the ace and subtract difference between 11 and 1 from the hand value
+    {
+        int handValue = 0;
+
+        foreach(DeckOfCards.Card handCard in hand){
+            handValue += handCard.GetCardHighValue();
+        }
+        if (handValue > 21) {
+                foreach(DeckOfCards.Card handCard in hand) {
+                    if (handCard.GetCardHighValue() == 11)
+                    {
+                        handValue -= 10;
+                    }
+                    if (handValue <= 21) {
+                        break;
+                    }
+                }
+        }
+        return handValue;
+    }
 }
