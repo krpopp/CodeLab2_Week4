1315d3f5344e73eb696467e1dfc0d2c18d6f62e9
NNNNateLiu <72626807+NNNNateLiu@users.noreply.github.com>
2022-09-27 12:00:42 -0400

    find out 6 bugs

---
+++ Assets/Students/Nate_Mischa/NMDealerHand.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 1, dealer only show hands when player bust - dealer should show hands whenever players loses (lose or bust)
+ * 2, when player hit 21, 'BlackJack' is not called - 'BlackJack' should be called when player hit 21
+ * 3, player loses when there is a tie - there should be a tie 
+ * 4, when dealer hit 21, 'BlackJack' is not called - 'BlackJack' should be called when dealer hit 21
+ * 5, Ace only has one value 11 - Ace should alternate between 1 and 11
+ * 6, no natural blackjack - there should be natural blackjack win or lose
+ */
+
+public class NMDealerHand : DealerHand
+{
+    
+}
