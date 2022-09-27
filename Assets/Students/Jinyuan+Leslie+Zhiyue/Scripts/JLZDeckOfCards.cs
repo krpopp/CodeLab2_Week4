c7ec9305a3257074161b6234a502437d6680a7c6
Zhiyue Huang <zhiyuehuang@yeah.net>
2022-09-27 11:23:11 -0400

    Bug Fixed: Use 4 decks of cards instead of 1 deck

--- Assets/Students/Jinyuan+Leslie+Zhiyue/Scripts/JLZDeckOfCards.cs
+++ Assets/Students/Jinyuan+Leslie+Zhiyue/Scripts/JLZDeckOfCards.cs
@@ -6,25 +6,20 @@ using Unity.VisualScripting;
 
 public class JLZDeckOfCards : DeckOfCards{
 
-	//Check the deck is empty or not
-	protected virtual bool IsValidDeck(){
-		return deck != null; 
-	}
 
-	//Add all kinds of cards to the deck
+	//Bug Fixed: Use 4 decks of cards instead of 1 deck
 	protected override void AddCardsToDeck(){
-		foreach (Card.Suit suit in Card.Suit.GetValues(typeof(Card.Suit))){
-			foreach (Card.Type type in Card.Type.GetValues(typeof(Card.Type))){
-				deck.Add(new Card(type, suit));
+		for(int i = 0; i < 4; i++)
+        {
+			foreach (Card.Suit suit in Card.Suit.GetValues(typeof(Card.Suit)))
+			{
+				foreach (Card.Type type in Card.Type.GetValues(typeof(Card.Type)))
+				{
+					deck.Add(new Card(type, suit));
+				}
 			}
 		}
+		
 	}
 	
-
-	//Draw a card
-	public virtual Card DrawCard(){
-		Card nextCard = deck.Next();
-
-		return nextCard;
-	}
 }
