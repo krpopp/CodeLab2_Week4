68b78b8aa25e2a371e11b466ac92cdb1b10d42c6
headlesscrown <mdh8533@nyu.edu>
2022-09-30 15:51:16 -0400

    added script to nmdeck

--- Assets/Students/Nate_Mischa/Scripts/NMDeckofCards.cs
+++ Assets/Students/Nate_Mischa/Scripts/NMDeckofCards.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class NMDeckOfCards : DeckOfCards
 {
-    protected override void AddCardsToDeck()
+    protected override void AddCardsToDeck() //build deck
     {
         for (int i = 0; i <= 3; i++)
         {
-            foreach (Card.Suit suit in Card.Suit.GetValues(typeof(Card.Suit))){
-                foreach (Card.Type type in Card.Type.GetValues(typeof(Card.Type))){
-                    deck.Add(new Card(type, suit));
+            foreach (Card.Suit suit in Card.Suit.GetValues(typeof(Card.Suit))) //add cards of this suit
+            {
+                foreach (Card.Type type in Card.Type.GetValues(typeof(Card.Type))) //add cards of this type
+                {
+                    deck.Add(new Card(type, suit)); //update card types and suits
                 }
             }
         }
@@ -17,19 +19,19 @@ public class NMDeckOfCards : DeckOfCards
 
     public int GetDeckCount()
     {
-        return deck.Count;
+        return deck.Count; //check deck count
     }
 
     //credit to Leslie's team code, remove card every time we draw
     public override Card DrawCard()
     {
-        Card nextCard = deck.Next();
+        Card nextCard = deck.Next(); //draw next card
 
-        deck.Remove(nextCard);
+        deck.Remove(nextCard); //remove previous
 
-        Debug.Log("Cards in Deck: " + deck.Count);
+        Debug.Log("Cards in Deck: " + deck.Count); //check deck count
 
-        return nextCard;
+        return nextCard; //add drawn card
     }
 
 }
