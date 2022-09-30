6cb020a966ad9dcc9082ab4441811c0931eff49d
headlesscrown <mdh8533@nyu.edu>
2022-09-30 15:56:12 -0400

    added comments to nmhand

--- Assets/Students/Nate_Mischa/Scripts/NMBlackJackHand.cs
+++ Assets/Students/Nate_Mischa/Scripts/NMBlackJackHand.cs
@@ -6,11 +6,11 @@ public class NMBlackJackHand : BlackJackHand
     public NMDealerHand dealerHand;
     protected override void ShowValue()
     {
-        handVals = GetHandValue();
+        handVals = GetHandValue(); //get player hand value
 			
-        total.text = "Player: " + handVals;
+        total.text = "Player: " + handVals; //player hand value text display
 
-        if(handVals > 21)
+        if(handVals > 21) //if player busts, hand exceeds 21
         {
             GameObject.Find("Game Manager").GetComponent<BlackJackManager>().PlayerBusted();
             //Bug1: called when player bust(dealer show hands value)
@@ -19,18 +19,18 @@ public class NMBlackJackHand : BlackJackHand
         //Bug2&6: call blackjack when player hit 21 or natural Blackjack
         else if (handVals == 21)
         {
-            GameObject.Find("Game Manager").GetComponent<BlackJackManager>().BlackJack();
+            GameObject.Find("Game Manager").GetComponent<BlackJackManager>().BlackJack(); //call GM
         }
     }
     
     public void ReSetUpHand()
     {
-        for (int i = 0; i < hand.Count; i++)
+        for (int i = 0; i < hand.Count; i++) //check all card obj in hand
         {
-            Destroy(handBase.transform.GetChild(i).gameObject);
+            Destroy(handBase.transform.GetChild(i).gameObject); //destroy cards in hand
         }
-        hand.Clear();
-        SetupHand();
+        hand.Clear(); //clear hand
+        SetupHand(); //reset hand
     }
     
 }
